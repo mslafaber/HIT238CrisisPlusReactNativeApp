9619ff026136a58e854a2405c345612965194529
DESKTOP-SDJCB40\mslaf <mslafaber1@gmail.com>
2020-09-24 06:02:25 +0930

    Created the Home Screen User Interface with the home screen components included

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
-import { StyleSheet, Text, View, Button, SafeAreaView, Alert } from 'react-native';
+import { StyleSheet, Text, View, Button, SafeAreaView, TouchableOpacity, Image, ScrollView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -10,18 +10,32 @@ const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator 
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#cc0000',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            fontSize: 30,
+            marginTop: 0,
+            marginBottom: 5
+          },
+          headerStatusBarHeight: 70
+        }}
+      >
         <Stack.Screen
           name="Home"
           component={HomeScreen}
-          options={{ title: 'CrisisPlus Home' }}
+          options={{ title: 'CrisisPlus' }}
         />
-        <Stack.Screen name="Life Threatening Emergency" component={triplezeroScreen} />
-        <Stack.Screen name="Poison Emergency" component={poisonScreen} />
-        <Stack.Screen name="Unsure Medical Emergency" component={unsuremedicalScreen} />
-        <Stack.Screen name="Natural Disaster Emergency" component={naturaldisasterScreen} />
-        <Stack.Screen name="Crime Reporting Emergency" component={crimereportingScreen} />
-        <Stack.Screen name="Mental Health Emergency" component={mentalhealthScreen} />
+        <Stack.Screen name="Life Threatening Emergency" component={triplezeroScreen} options={{ title: 'CrisisPlus' }}/>
+        <Stack.Screen name="Poison Emergency" component={poisonScreen} options={{ title: 'CrisisPlus' }}/>
+        <Stack.Screen name="Unsure Medical Emergency" component={unsuremedicalScreen} options={{ title: 'CrisisPlus' }}/>
+        <Stack.Screen name="Natural Disaster Emergency" component={naturaldisasterScreen} options={{ title: 'CrisisPlus' }}/>
+        <Stack.Screen name="Crime Reporting Emergency" component={crimereportingScreen} options={{ title: 'CrisisPlus' }}/>
+        <Stack.Screen name="Mental Health Emergency" component={mentalhealthScreen} options={{ title: 'CrisisPlus' }}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -29,49 +43,93 @@ const App = () => {
 
 export default App;
 
+const Separator = () => (
+  <View style={styles.separator} />
+);
+
 const HomeScreen = ({ navigation }) => {
   return (
-    <View>
-      <Text style={styles.titlebar}>
-        CrisisPlus
-      </Text>
-      <Button
-        title="Go to Life Threatening Emergency Contact"
-        onPress={() =>
-          navigation.navigate('Life Threatening Emergency', { name: 'Life Threatening Emergency' })
-        }
-      />
-      <Button
-        title="Go to Poison Emergency Contact"
-        onPress={() =>
-          navigation.navigate('Poison Emergency', { name: 'Poison Emergency' })
-        }
-      />
-      <Button
-        title="Go to Unsure Medical Emergency contact"
-        onPress={() =>
-          navigation.navigate('Unsure Medical Emergency', { name: 'Unsure Medical Emergency' })
-        }
-      />
-      <Button
-        title="Go to Natural Disaster Emergency Contact"
-        onPress={() =>
-          navigation.navigate('Natural Disaster Emergency', { name: 'Natural Disaster Emergency' })
-        }
-      />
-      <Button
-        title="Go to Crime Reporting Emergency Contact"
-        onPress={() =>
-          navigation.navigate('Crime Reporting Emergency', { name: 'Crime Reporting Emergency' })
-        }
-      />
-      <Button
-        title="Go to Mental Health Emergency Contact"
-        onPress={() =>
-          navigation.navigate('Mental Health Emergency', { name: 'Mental Health Emergency' })
-        }
-      />
-    </View>
+    <ScrollView style={styles.container}>
+      <Separator />
+      <View style={styles.buttonsection}>
+        <TouchableOpacity 
+          style={styles.buttonstyle}
+          activeOpacity={0.4}
+          onPress={() =>
+            navigation.navigate('Life Threatening Emergency', { name: 'Life Threatening Emergency' })
+          }
+        >
+          <Text style={styles.buttonTextStyle}>Go to Life Threatening Emergency Contact</Text>
+          <Image source={require("./assets/triple-zero.jpg")} style={styles.buttonImageIconStyle}/>
+        </TouchableOpacity>
+      </View>
+      <Separator />
+      <View style={styles.buttonsection}>
+        <TouchableOpacity 
+          style={styles.buttonstyle}
+          activeOpacity={0.4}
+          onPress={() =>
+            navigation.navigate('Poison Emergency', { name: 'Poison Emergency' })
+          }
+        >
+          <Text style={styles.buttonTextStyle}>Go to Poison Emergency Contact</Text>
+          <Image source={require("./assets/poison.png")} style={styles.buttonImageIconStyle}/>
+        </TouchableOpacity>
+      </View>
+      <Separator />
+      <View style={styles.buttonsection}>
+        <TouchableOpacity 
+          style={styles.buttonstyle}
+          activeOpacity={0.4}
+          onPress={() =>
+            navigation.navigate('Unsure Medical Emergency', { name: 'Unsure Medical Emergency' })
+          }
+        >
+          <Text style={styles.buttonTextStyle}>Go to Unsure Medical Emergency Contact</Text>
+          <Image source={require("./assets/health-direct.jpg")} style={styles.buttonImageIconStyle}/>
+        </TouchableOpacity>
+      </View>
+      <Separator />
+      <View style={styles.buttonsection}>
+        <TouchableOpacity 
+          style={styles.buttonstyle}
+          activeOpacity={0.4}
+          onPress={() =>
+            navigation.navigate('Natural Disaster Emergency', { name: 'Natural Disaster Emergency' })
+          }
+        >
+          <Text style={styles.buttonTextStyle}>Go to Natural Disaster Emergency Contact</Text>
+          <Image source={require("./assets/floods.jpg")} style={styles.buttonImageIconStyle}/>
+        </TouchableOpacity>
+      </View>
+      <Separator />
+      <View style={styles.buttonsection}>
+        <TouchableOpacity 
+          style={styles.buttonstyle}
+          activeOpacity={0.4}
+          onPress={() =>
+            navigation.navigate('Crime Reporting Emergency', { name: 'Crime Reporting Emergency' })
+          }
+        >
+          <Text style={styles.buttonTextStyle}>Go to Crime Reporting Emergency Contact</Text>
+          <Image source={require("./assets/crime-report.png")} style={styles.buttonImageIconStyle}/>
+        </TouchableOpacity>
+      </View>
+      <Separator />
+      <View style={styles.buttonsection}>
+        <TouchableOpacity 
+          style={styles.buttonstyle}
+          activeOpacity={0.4}
+          onPress={() =>
+            navigation.navigate('Mental Health Emergency', { name: 'Mental Health Emergency' })
+          }
+        >
+          <Text style={styles.buttonTextStyle}>Go to Mental Health Emergency Contact</Text>
+          <Image source={require("./assets/mental-health.png")} style={styles.buttonImageIconStyle}/>
+        </TouchableOpacity>
+      </View>
+      <Separator />
+    </ScrollView>
   );
 };
 
@@ -79,7 +137,7 @@ const triplezeroScreen = () => {
   return (
     <View>
       <Text style={styles.titlebar}>
-        CrisisPlus
+        Life Threatening Emeregency
       </Text>
       <Text>This is Life Threatening Emeregency Contact screen</Text>
     </View>
@@ -90,7 +148,7 @@ const poisonScreen = () => {
   return (
     <View>
       <Text style={styles.titlebar}>
-        CrisisPlus
+        Poison Emergency
       </Text>
       <Text>This is Poison Emergency Contact screen</Text>
     </View>
@@ -101,7 +159,7 @@ const unsuremedicalScreen = () => {
   return (
     <View>
       <Text style={styles.titlebar}>
-        CrisisPlus
+        Unsure Medical Emergency
       </Text>
       <Text>This is Unsure Medical Emergency Contact screen</Text>
     </View>
@@ -112,7 +170,7 @@ const naturaldisasterScreen = () => {
   return (
     <View>
       <Text style={styles.titlebar}>
-        CrisisPlus
+        Natural Disaster Emergency
       </Text>
       <Text>This is Natural Disaster Emergency Contact screen</Text>
     </View>
@@ -123,7 +181,7 @@ const crimereportingScreen = () => {
   return (
     <View>
       <Text style={styles.titlebar}>
-        CrisisPlus
+        Crime Reporting Emergency
       </Text>
       <Text>This is Crime Reporting Emergency Contact screen</Text>
     </View>
@@ -134,7 +192,7 @@ const mentalhealthScreen = () => {
   return (
     <View>
       <Text style={styles.titlebar}>
-        CrisisPlus
+        Mental Health Emergency
       </Text>
       <Text>This is Mental Health Emergency Contact screen</Text>
     </View>
@@ -144,7 +202,7 @@ const mentalhealthScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignContent: 'flex-start',
+    alignContent: 'center',
     marginHorizontal: 16,
   },
   title: {
@@ -152,10 +210,45 @@ const styles = StyleSheet.create({
     marginVertical: 8,
   },
   titlebar: {
-    color: "#fff",
-    backgroundColor: "#cc0000",
-    fontSize: 60,
+    fontSize: 18,
     textAlign: "center",
     padding: 22
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  buttonsection: {
+    marginTop: 10,
+  },
+  buttonstyle: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    backgroundColor: '#F5FFFA',
+    borderRadius: 15,
+    padding: 0,
+    marginBottom: 5,
+    shadowColor: '#303838',
+    shadowOffset: { width: 0, height: 5 },
+    shadowRadius: 10,
+    shadowOpacity: 0.4,
+  },
+  buttonImageIconStyle: {
+    padding: 5,
+    marginTop: 10,
+    marginLeft: 5,
+    marginRight: 5,
+    marginBottom: 20,
+    height: 150,
+    width: 350,
+    resizeMode: 'stretch',
+  },
+  buttonTextStyle: {
+    fontSize: 14,
+    marginTop: 15,
+    marginBottom: 4,
+    marginLeft: 10,
+    fontWeight: "bold"
   }
 });
